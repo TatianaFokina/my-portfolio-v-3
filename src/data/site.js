e953797c6a5aa9f814ac32637f3cf9f4545dbf2e
TatianaFokina <fokinatatian@gmail.com>
2021-05-06 02:47:58 +0300

    Add links.json & site.js

--- src/data/site.js
+++ src/data/site.js
@@ -1,6 +1,5 @@
 module.exports = {
-	'title': '/',
-	'subTitle': '/',
-	'description': '/',
+	'siteLink': 'https://fokina-dev.ru',
+	'description': 'Портфолио HTML-верстальщицы Тани Фокиной. Информация о ней, её навыках, проектах и активностях.',
 	'currentYear': new Date().getFullYear(),
 };
