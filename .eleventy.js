7b9fb36ed7f64571670c3f3f2c8129442a23eb55
TatianaFokina <fokinatatian@gmail.com>
2021-05-11 01:54:33 +0300

    Add short code into Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const pluginSass = require("eleventy-plugin-sass");
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/fonts');
 	eleventyConfig.addPassthroughCopy('src/scripts');
-	//eleventyConfig.addPassthroughCopy({ "**/*.jpg": "img" });
+	eleventyConfig.addPassthroughCopy({ "src/**/*.{svg,jpg,png}": "images" });
 
 	// Styles
 	eleventyConfig.addPlugin(pluginSass, {
@@ -13,6 +13,22 @@ module.exports = function(eleventyConfig) {
 		],
 		outputDir: 'test/styles',
 		cleanCSS: false
+	});
+
+	// Shortcode
+	eleventyConfig.addPairedShortcode("project", function(titleFirst, titleSecond) {
+		return `
+			<div>
+				<div>
+					<div>
+						<h2 class="project-desc__heading">${titleFirst}</h2>
+					</div>
+					<div>
+						<h2 class="project-desc__heading">${titleSecond}</h2>
+					</div>
+				</div>
+			</div>
+			`;
 	});
 
 	return {
