87511afbccd0c631edb79a5b45080461e9dc34f8
TatianaFokina <fokinatatian@gmail.com>
2021-05-29 02:42:38 +0300

    Change link for OG image

--- src/data/site.js
+++ src/data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"siteLink": "https://fokina-dev.ru",
 	"description": "Портфолио HTML-верстальщицы Тани Фокиной. Информация о ней, её навыках, проектах и активностях.",
-	"ogImageLink": "https://fokina-dev.ru/images/og-image.jpg",
+	"ogImageLink": "https://fokina-dev.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
