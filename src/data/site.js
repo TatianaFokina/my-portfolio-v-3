6908435f7c50f6886bb7bf486d29b910ebd9ecb2
TatianaFokina <fokinatatian@gmail.com>
2021-07-04 23:48:47 +0300

    Replace title, fix head

--- src/data/site.js
+++ src/data/site.js
@@ -1,5 +1,6 @@
 module.exports = {
 	"siteLink": "https://fokina-dev.ru",
+	"title": "Фокина Татьяна — портфолио",
 	"description": "Портфолио веб-разработчицы Тани Фокиной. Информация о ней, её навыках, проектах и активностях.",
 	"ogImageLink": "https://fokina-dev.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
