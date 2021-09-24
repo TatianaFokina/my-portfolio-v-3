e916bd44f3fa4debd74834aca82ed4bd83683c52
TatianaFokina <fokinatatian@gmail.com>
2021-09-24 22:37:33 +0300

    Fix menu script

--- src/scripts/burger-menu/burger-menu.js
+++ src/scripts/burger-menu/burger-menu.js
@@ -1,15 +1,16 @@
 export function BurgerMenu() {
-	let $menuBtn = document.querySelector('.nav__button'),
-		$menuContainer = document.querySelector('.nav__menu'),
-		$menuBtn_closed = 'js-nav__button--close',
-		$menuContainer_opened = 'js-nav__menu--open',
-		$isOpen = false;
+	const $menuBtn = document.querySelector(".menu__button"),
+		$menuContainer = document.querySelector(".menu__list"),
+		$menuBtn_closed = "js-menu__button--close",
+		$menuContainer_opened = "js-menu__list--open";
+
+	let $isOpen = false;
 
 	// Open menu
 	function openMenu() {
 		$menuBtn.classList.add($menuBtn_closed);
-		$menuBtn.setAttribute('aria-expanded', 'true');
-		$menuBtn.setAttribute('aria-label', 'Закрыть меню');
+		$menuBtn.setAttribute("aria-expanded", "true");
+		$menuBtn.setAttribute("aria-label", "Закрыть меню");
 		$menuContainer.classList.add($menuContainer_opened);
 		$isOpen = true;
 	}
@@ -17,21 +18,21 @@ export function BurgerMenu() {
 	// Close menu
 	function closeMenu() {
 		$menuBtn.classList.remove($menuBtn_closed);
-		$menuBtn.setAttribute('aria-expanded', 'false');
-		$menuBtn.setAttribute('aria-label', 'Открыть меню');
+		$menuBtn.setAttribute("aria-expanded", "false");
+		$menuBtn.setAttribute("aria-label", "Открыть меню");
 		$menuContainer.classList.remove($menuContainer_opened);
 		$isOpen = false;
 	}
 
 	if ($menuBtn) {
-		$menuBtn.addEventListener('click', function(e) {
+		$menuBtn.addEventListener("click", function(e) {
 			e.stopPropagation();
 			e.preventDefault();
 
 			if ($isOpen === false) {
 				openMenu();
 				// Close menu for click to the document / menu item
-				window.addEventListener('click', function(event) {
+				window.addEventListener("click", function(event) {
 					if (!$menuBtn.contains(event.target)) {
 						closeMenu();
 					}
