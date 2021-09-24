9668fe99ad2dd98a437f1d6b9111dc799820afdc
TatianaFokina <fokinatatian@gmail.com>
2021-09-24 23:41:09 +0300

    Change a bracket

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,4 @@
-import { KeyboardFocus } from './focus/keyboard-focus.js';
+import { KeyboardFocus } from "./focus/keyboard-focus.js";
 import { BurgerMenu } from "./burger-menu/burger-menu.js";
 
 KeyboardFocus();
