83b133b2c178ea74639f1cf8f6eab18d6400a869
TatianaFokina <fokinatatian@gmail.com>
2022-03-07 23:12:57 +0300

    Translate a content from the main page to English

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://fokina-dev.ru",
-	"title": "Фокина Татьяна — портфолио",
-	"description": "Портфолио веб-разработчицы Тани Фокиной. Информация о ней, её навыках, проектах и активностях.",
+	"title": "Tatiana Fokina Portfolio",
+	"description": "Portfolio of Tatiana Fokina, a web-developer. Information about her, her skills, pet-projects and activities.",
 	"ogImageLink": "https://fokina-dev.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
