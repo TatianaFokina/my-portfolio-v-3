6e68289ad2dca1bccf5aae859c6584a992b88a4b
TatianaFokina <fokinatatian@gmail.com>
2022-03-10 21:50:51 +0300

    Fix a few texts

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://fokina-dev.ru",
-	"title": "Tatiana Fokina Portfolio",
-	"description": "Portfolio of Tatiana Fokina, a web-developer. Information about her, her skills, pet-projects and activities.",
+	"title": "Tatiana Fokina - web developer",
+	"description": "Tatiana Fokina is a web developer. Information about her, her skills, pet-projects and activities.",
 	"ogImageLink": "https://fokina-dev.ru/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
